4c80951cff645e42e921163d16e12b8893fc22aa
wangkevin20 <71090255+wangkevin20@users.noreply.github.com>
2020-11-14 14:24:55 -0500

    week9 ex3

---
+++ week9ex3.js
@@ -0,0 +1,9 @@
+
+function add()
+{
+  var a,b,c;
+  a=Number(document.getElementById("first").value);
+  b=Number(document.getElementById("second").value);
+  c= a + b;
+  document.getElementById("output").value= c;
+}
